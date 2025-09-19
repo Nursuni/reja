d54564aaef136d32aae590ecd27300b562df9361
Your Name <your@email.com>
2025-09-19 19:19:51 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,30 +2,35 @@ console.log("Salom, dunyo!");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-// 1: Kirish codelari
-app.use(express.static("public")); //kirib kelyabgan rquestlar uchun public folder ochiq css+ HTML
-app.use(express.json()); //json formatdagi malumotlarni qabul qilish uchun
-app.use(express.urlencoded({ extended: true })); //htmldan traditionaldan form requestlarni qabul qilish uchun
-
-// 2: Session code
-// 3: View codes
-app.set("views", "views");
-app.set("view engine", "ejs"); //html frontendni backendda ishlatish uchun //BSSRendering engine
-//Single Page Application (SPA) - bitta html fayl
-//Multi Page Application (MPA) - ko'p html faylgi
-
-// 4: Routing codes
-app.post("/create-item", (req, res) => {
-  console.log(req.body);
-  res.json({ test: "success" });
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
 
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.post("/create-item", (req, res) => {
+  // TODO: code with db here;
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 app.get("/", function (req, res) {
   res.render("harid");
 });
 
-// Server setup
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
