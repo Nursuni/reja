3ed9a2e2cb541c00603e67d7163c9642da242eb3
Your Name <your@email.com>
2025-09-30 14:57:55 +0900

    feat: FrontEnd JavaScript ni qurish

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ console.log("Salom, dunyo!");
 const express = require("express");
 const app = express();
 
-// const fs = require("fs");
+const fs = require("fs");
 
 //MongoDB chaqirish
 const db = require("./server").db();
 
 // let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 //1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -27,19 +27,15 @@ app.post("/create-item", (req, res) => {
   console.log("user entered/ create-item");
   const new_reja = req.body.reja;
   db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.end("successfully addedd");
-    }
+    console.log(data.ops);
+    res.json(data.ops[0]);
   });
-  // TODO: code with db here;
 });
 
 app.get("/author", (req, res) => {
   res.render("author", { user: user });
 });
+
 app.get("/", function (req, res) {
   console.log("user entered /");
   db.collection("plans")
